5f5b6a41c89758b303288aa8723c7582136e4ea5
goutham120 <nagilagoutham98@gmail.com>
2026-02-13 18:55:27 +0530

    front side

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,33 +1,31 @@
 import Upload from "./pages/Upload";
 import Notes from "./pages/Notes";
-
 import Leaderboard from "./pages/Leaderboard";
 import Dashboard from "./pages/Dashboard";
 import "./App.css";
 
 export default function App() {
   return (
-    <div className="container">
-      <div className="navbar">
-        📚 Acadexx360 - Smart Notes Hub
-      </div>
+    <div className="app">
+      <div className="navbar">📚 Acadexx360 - Smart Notes Hub</div>
 
-      <div className="section">
-        <Upload />
-      </div>
+      <div className="main-content">
+        <div className="section">
+          <Upload />
+        </div>
 
-      <div className="section">
-        <Notes />
-      </div>
+        <div className="section">
+          <Notes />
+        </div>
 
-      <div className="section">
-        <Leaderboard />
-      </div>
+        <div className="section">
+          <Leaderboard />
+        </div>
 
-      <div className="section">
-        <Dashboard />
+        <div className="section">
+          <Dashboard />
+        </div>
       </div>
     </div>
   );
 }
-
